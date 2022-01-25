404c7174dc18cade4d61f45edd0193dfa4817fed
80qah <92930382+80qah@users.noreply.github.com>
2022-01-25 16:43:19 +0800

    Add files via upload

---
+++ Main2.java
@@ -0,0 +1,20 @@
+public class Main2 {
+    public static void main(String [] args){
+        String module="CSC1009";
+        switch(module){
+            case "CSC1009":
+                System.out.println("Object-Oriented Programming");
+                break;
+            case "CSC1008":
+                System.out.println("Operating Systems");
+                break;
+            case "CSC1007":
+                System.out.println("Data Structure");
+                break;
+            case "CSC1006":
+                System.out.println("Mathematics");
+                break;
+            
+        }
+    }
+}
---
+++ Main3.java
@@ -0,0 +1,9 @@
+public class Main3 {
+    public static void main(String [] args){
+        for(int x=102;x>66;x=x-1){
+            if(x%2==1){
+                System.out.println("The odd number:" + x);
+            }
+        }
+    }
+}
